42920e5a3cc4e6c31863b7a2af172b5805f57c43
Dmitry Kolomiets <kolomiets@gmail.com>
2016-05-24 12:47:29 +0300

    Remove unneeded files.

--- Atalasoft.Demo.WebDocumentViewer/WebViewingDemoControl.ascx.cs
+++ Atalasoft.Demo.WebDocumentViewer/WebViewingDemoControl.ascx.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-//using CMS.PortalControls
 
-//public partial class WebViewingDemoControl : CMSAbstractWebPart
-public partial class WebViewingDemoControl : System.Web.UI.UserControl
+public class WebViewingDemoControl : System.Web.UI.UserControl
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
     }
